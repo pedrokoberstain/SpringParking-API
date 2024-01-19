2fc823546b062440410701f686a71aa55a0e75c5
Kober <pedrokoberstain2@gmail.com>
2024-01-18 22:58:38 -0300

    Mudança de métodos para uso de DTO (Data Transfer Object)

--- src/main/java/com/kober/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/kober/demoparkapi/web/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.kober.demoparkapi.web.controller;
 
+import com.kober.demoparkapi.web.dto.mapper.UsuarioMapper;
 import com.kober.demoparkapi.entity.Usuario;
 import com.kober.demoparkapi.service.UsuarioService;
+import com.kober.demoparkapi.web.dto.UsuarioCreateDto;
+import com.kober.demoparkapi.web.dto.UsuarioResponseDto;
+import com.kober.demoparkapi.web.dto.UsuarioSenhaDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,27 +21,26 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
-        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
-        return ResponseEntity.ok(user);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+        Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+        return ResponseEntity.noContent().build();
     }
 
-
     @GetMapping
-    public ResponseEntity<List<Usuario>> getAll() {
+    public ResponseEntity<List<UsuarioResponseDto>> getAll() {
         List<Usuario> users = usuarioService.buscarTodos();
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(UsuarioMapper.toListDto(users));
     }
 }
---
+++ src/main/java/com/kober/demoparkapi/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,10 @@
+package com.kober.demoparkapi.web.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UsuarioCreateDto {
+
+    private String username;
+    private String password;
+}
