d8a5f658eb4fcf1166e7fd700bf1a75be5542b57
Kober <pedrokoberstain2@gmail.com>
2024-01-19 11:59:07 -0300

    Refatoração dos testes para uso de autenticação e permissões de acessos

--- src/main/java/com/kober/demoparkapi/web/controller/AutenticacaoController.java
+++ src/main/java/com/kober/demoparkapi/web/controller/AutenticacaoController.java
@@ -1,9 +1,9 @@
 package com.kober.demoparkapi.web.controller;
 
-import com.kober.demoparkapi.web.exception.ErrorMessage;
 import com.kober.demoparkapi.jwt.JwtToken;
 import com.kober.demoparkapi.jwt.JwtUserDetailsService;
 import com.kober.demoparkapi.web.dto.UsuarioLoginDto;
+import com.kober.demoparkapi.web.exception.ErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
